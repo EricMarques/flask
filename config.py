23e4499ab13945921500a986b2c5fc2d9d3692f8
Eric Marques <erc_m@hotmail.com>
2020-03-04 22:16:17 -0300

    Ajustes repositório

--- config.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-
-#mysql://nomeUsuario:senhaUsuario@servidor/nomeBancoDados
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/aulaFlask'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-SECRET_KEY = 'root'
